353a72ad9525f046578bbd10988ec4dcf291a721
Taloreff-hls <talo@moveohls.com>
2024-09-25 11:36:01 +0300

    refactor(custom-hook): Exported infinite scroll logic to a custom hook

--- src/cmps/PokemonGrid.tsx
+++ src/cmps/PokemonGrid.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import PokemonCardComponent from "./PokemonCard";
 import { Pokemon } from "../interfaces/Pokemon";
 import { CardGrid, EmptyPokemonsContainer } from "../styles/StyledPokemonCard";
 import PokemonModal from "./PokemonModal";
 import { EmptyPokemons } from "../styles/StyledTable";
 import emptyPokemons from "../assets/imgs/no_pokemon.png";
+import { useInfiniteScroll } from "../hooks/useInfiniteScrollHook";
 
 interface PokemonGridProps {
   pokemons: Pokemon[];
@@ -20,28 +21,11 @@ const PokemonGrid = ({
   isFetchingNextPage,
 }: PokemonGridProps) => {
   const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
-  const observerRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
-          fetchNextPage();
-        }
-      },
-      { threshold: 1.0 }
-    );
-
-    if (observerRef.current) {
-      observer.observe(observerRef.current);
-    }
-
-    return () => {
-      if (observerRef.current) {
-        observer.unobserve(observerRef.current);
-      }
-    };
-  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+  const observerRef = useInfiniteScroll({
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  });
 
   const handleCardClick = (pokemon: Pokemon) => {
     setSelectedPokemon(pokemon);
@@ -68,9 +52,7 @@ const PokemonGrid = ({
         )}
         <div ref={observerRef} style={{ height: "20px" }} />
       </CardGrid>
-
       {isFetchingNextPage && <div>Loading more...</div>}
-
       {selectedPokemon && (
         <PokemonModal
           pokemon={selectedPokemon}
